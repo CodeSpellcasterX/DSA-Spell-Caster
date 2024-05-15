8440ceda7c1af571508fb537f91d69d7e8460fd9
CodeSpellcasterX <71221999+CodeSpellcasterX@users.noreply.github.com>
2024-05-15 13:44:52 +0530

    Time: 0 ms (100%), Space: 44.9 MB (86.41%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int ans=0;
-        for(int i=0;i<nums.length;i++){
-            ans=ans^nums[i];
+        int ans = 0;
+        int i = 0;
+        while (i < nums.length) {
+            ans = ans ^ (i + 1 ) ^ nums[i];
+            i++;
         }
-        
-         for(int i=1;i<=nums.length;i++){
-            ans=ans^i;
-        }
-        
         return ans;
-        
     }
 }
