dc1218dd89f5ddb68303f4dca0f3edf58f37c853
CodeSpellcasterX <71221999+CodeSpellcasterX@users.noreply.github.com>
2024-05-17 00:34:14 +0530

    Time: 1 ms (99.78%), Space: 53.6 MB (6.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,16 +3,21 @@ class Solution {
         int num = nums[0];
         int count = 1;
         int i = 1;
-        while (i<nums.length) {
-            if (nums[i]== num) {
+        while(i<nums.length) {
+            if(nums[i]==num) {
                 count++;
             } else {
                 if(count==0) {
-                    count++;
+                    count = 1;
                     num = nums[i];
                 } else {
                     count--;
                 }
+                
+            }
+
+            if(count == 0) {
+                num = nums[i];
             }
             i++;
         }
