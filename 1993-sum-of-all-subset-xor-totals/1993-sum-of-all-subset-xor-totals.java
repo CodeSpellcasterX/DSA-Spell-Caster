d0323d5f2ae227d85132ecc18be24d795dee82b8
CodeSpellcasterX <71221999+CodeSpellcasterX@users.noreply.github.com>
2025-04-05 23:44:46 +0530

    Time: 6 ms (21.45%), Space: 41 MB (54.42%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -1,5 +1,23 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
         
+        int i = 1;
+        int ans = 0 ;
+        for(;i<=Math.pow(2,nums.length)-1;i++){
+            // int val = 1;
+            int temp = i;
+            int idx = nums.length-1;
+            int sub = 0;
+            while(temp!=0){
+                int get = 1&temp;
+                if(get==1){
+                    sub=sub^nums[idx];
+                }
+                idx--;
+                temp=temp>>1;
+            }
+            ans= ans+sub;
+        }
+        return ans;
     }
 }
